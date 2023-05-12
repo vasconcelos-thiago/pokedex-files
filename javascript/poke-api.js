5c5bbacd206b466f023393d464bf4ccda5a836ec
vasconcelos-thiago <vasconcelosthx@gmail.com>
2023-05-12 01:28:02 -0300

    insertion of pokemon object

--- javascript/poke-api.js
+++ javascript/poke-api.js
@@ -3,10 +3,27 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+const convertPokeApiDetail =(pokeDetail) =>{
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const {type} = types
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+
 }
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetail)
+}
 pokeApi.getPokemons = (offset=0, limit =12) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
--- javascript/pokedex.js
+++ javascript/pokedex.js
@@ -1,22 +1,16 @@
 
 
-
-const convertTypes = (pokemonTypes) => {
-    return pokemonTypes.map((typeSlot) => `<span>${typeSlot.type.name}</span>`
-    )
-}
-
 const convertPokemonToLi = (pokemon) => {
     return `                <li class="pokemon">
     <div class="id">
         <span>${pokemon.name}</span>
-        <span>${pokemon.order}</span>
+        <span>${pokemon.number}</span>
     </div>
     <div class="imgs">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.order}.svg" alt="${pokemon.name}">
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
     </div>
     <div class="types">
-        ${convertTypes(pokemon.types).join('')}
+        ${pokemon.types.map((type)=> `<span>${type}</span>`).join('')}
     </div>
 
 </li>`
